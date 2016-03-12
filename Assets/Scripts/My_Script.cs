a6b051a93e74341595d3ad08722be2f6b55154f9
NuggeTsMaker <jmberg511@gmail.com>
2016-03-12 15:04:24 +0100

    8D

--- Assets/Scripts/My_Script.cs
+++ Assets/Scripts/My_Script.cs
@@ -1,30 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
 public class My_Script : MonoBehaviour
 {
-    [Range(0.0f,100.0f)]
+    [Range(0.0f, 100.0f)]
     public float imABitch;
-    void Start ()
+    void Start()
     {
         //lol
-	}
+    }
 
-	void Update ()
+    void Update()
     {
-	
-=======
-public class My_Script : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log ("ta mere");	
->>>>>>> origin/master
-	}
+        Debug.Log("ta mere");
+    }
 }
