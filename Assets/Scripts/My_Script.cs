c3eb73e48a99c1b496aab925400086e268ce62d8
antoineBoisnault <antoine.boisnault@hotmail.fr>
2016-03-12 14:55:20 +0100

    ftu
    xfg

--- Assets/Scripts/My_Script.cs
+++ Assets/Scripts/My_Script.cs
@@ -5,11 +5,11 @@ public class My_Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("ta mere");	
 	}
 }
